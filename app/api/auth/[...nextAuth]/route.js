4bc7757b5d00e0edb28c7c99c2ca043ffa8e014d
SanskarGithub07 <117458101+SanskarGithub07@users.noreply.github.com>
2024-01-18 16:31:09 +0530

    Added middleware.

---
+++ app/api/auth/[...nextAuth]/route.js
@@ -0,0 +1,29 @@
+import  CredentialsProvider  from "next-auth/providers/credentials";
+import NextAuth from "next-auth/next";
+
+export const authOptions = {
+    pages:{
+        signIn: "/loginPage"
+    },
+    providers: [
+        CredentialsProvider({
+            name: "credentials",
+            credentials: {},
+            async authorize(credentials){
+                try{
+                    const user = await login(credentials);
+                    console.log("This is the user= ", user);
+                    return user;
+                }
+                catch(error){
+                    console.log("Error = ", error);
+                    return null;
+                }
+            },
+        }),
+    ],
+};
+
+const handler = NextAuth(authOptions);
+
+export {handler as GET, handler as POST};
